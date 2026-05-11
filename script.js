91db185ad0a95d9f53caec66c48deb19c1057907
jotaxh <jhonataribeiro905@gmail.com>
2026-05-11 11:37:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrirInsta() {
+    window.open('https://www.instagram.com/jotah58?igsh=Z3J2YjZjMGlueHRz', '_blank');
+}
+
+function abrirZap() {
+    window.open('https://wa.me/5568992151741?text=Olá,%20vim%20pelo%20seu%20portfólio', '_blank');
+}
+
+function abrirGit() {
+    window.open('https://github.com/settings/profile', '_blank');
+}
