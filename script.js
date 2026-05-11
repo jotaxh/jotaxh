412a0eaccf48c23d45f55cde4b1b3a9a09adb8bb
jotaxh <jhonataribeiro905@gmail.com>
2026-05-11 19:24:52 -0300

    Add comments to social media opening functions
    Added comments to clarify the purpose of each function.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+//Abre o Instrgram em uma nova aba
 function abrirInsta() {
     window.open('https://www.instagram.com/jotah58?igsh=Z3J2YjZjMGlueHRz', '_blank');
 }
-
+//Abre o Whatsapp com msg automático
 function abrirZap() {
     window.open('https://wa.me/5568992151741?text=Olá,%20vim%20pelo%20seu%20portfólio', '_blank');
 }
-
+//Abre o GitHub em uma nova aba
 function abrirGit() {
     window.open('https://github.com/settings/profile', '_blank');
 }
